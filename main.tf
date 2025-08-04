3556f664ab920d99f9a843ce57e45fb214db71c3
phanee9550015151 <85539662+phanee9550015151@users.noreply.github.com>
2025-08-04 13:28:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami = "ami-0fb0b230890ccd1e6"  # Ubuntu 20.04 - 2025-06-24
+"
   instance_type = "t2.micro"
 }
